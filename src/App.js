4e9e8d096cb81629fa37efdff771a55b7b5d9bbe
Moses <moseschawawa@gmail.com>
2021-05-25 14:03:55 +0200

    Looked at different usecase of useEffect inside a component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function App() {
 
+    // count is immutable
+    // setCount trigger the component ot rerender
     const [count, setCount] = useState(0)
 
+    /**
+     * This type of use effect runs only once, thus the first time componets
+     * get mounted
+     */
+    useEffect(() => {
+        console.log('runs onlyonce')
+        // The return function is optional 
+        return () => {
+            // Respsonsible for cleaning up when the component unmounts
+            // Runs only once when the component unmout
+            console.log('cleaning up everytime')
+        }
+    }, []);
+
+    /**
+     * This type of use effect renders everytime this component reloads
+     * as well as when @setCount is triggered
+     */
+    useEffect(() => {
+        console.log('runs everytime')
+        // The return function is optional 
+        return () => {
+            // Respsonsible for cleaning up when the component unmounts
+            // Runs everytime
+            console.log('cleaning up everytime')
+        }
+    });
+
+
+
+
+
     return (
+        /**
+         * Always make sure that you inclose the JSX with the parathesis
+         */
         <div>
             <h1>Count : {count}</h1>
             <button onClick={() => setCount(count + 1)}>Increment</button>
