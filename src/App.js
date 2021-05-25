34fb5a7beded6a254f98b2df903dc6ced1beef5f
Moses <moseschawawa@gmail.com>
2021-05-25 14:11:51 +0200

    Added useEffect with a depended variable 'count' 😊

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect /* Runs in order as well */ } from 'react'
+
+
 
 export default function App() {
 
     // count is immutable
     // setCount trigger the component ot rerender
     const [count, setCount] = useState(0)
+
+
+    /**
+     * This type of use effect runs only when the variable count changes, thus the first time componets
+     * get mounted
+     */
+    useEffect(() => {
+        console.log('count changed')
+        // The return function is optional 
+        return () => {
+            // Respsonsible for cleaning up when the component unmounts
+            // Runs only once when the component unmout
+            console.log('cleaning after count')
+        }
+    }, [count]);
 
     /**
      * This type of use effect runs only once, thus the first time componets
